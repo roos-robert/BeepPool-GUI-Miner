321b2c3f80dc51a2f8ccf6bb3fb0c447cbbb74b8
Robert Roos <rr222cy@student.lnu.se>
2018-05-05 18:39:50 +0200

    V0.0.1

---
+++ BeepPool-Windows-Miner/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BeepPool_Windows_Miner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                string[] lines = System.IO.File.ReadAllLines(@"settings.txt");
+
+                if (lines.Length == 3)
+                {
+                    textBox3.Text = lines[0];
+                    textBox1.Text = lines[1];
+                    textBox2.Text = lines[2];
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var wallet = textBox3.Text;
+            var cores = textBox1.Text;
+            var worker = textBox2.Text;
+
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = @"miner.exe";
+            startInfo.Arguments = String.Format("miner --miner={0} --wallet-address=\"{1}\" --extra-data=\"{2}\"", cores, wallet, worker);
+            Process.Start(startInfo);
+
+            using (StreamWriter bw = new StreamWriter(File.Create("settings.txt")))
+            {
+                bw.WriteLine(wallet);
+                bw.WriteLine(cores);
+                bw.WriteLine(worker);
+                bw.Close();
+            }
+        }
+    }
+}
